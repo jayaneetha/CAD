fd120b557b025bf0de42c2f8a7425d85f1441af2
Thejan Rajapakshe <coder.clix@gmail.com>
2015-08-11 11:48:50 +0530

    Init

--- application/views/partial/mail_notification.php
+++ application/views/partial/mail_notification.php
@@ -19,7 +19,7 @@
     ?>
     <li>
         <div class="text-center link-block">
-            <a href="mailbox.html">
+            <a href="/index.php/compose">
                 <i class="fa fa-envelope"></i> <strong>Read All Messages</strong>
             </a>
         </div>
