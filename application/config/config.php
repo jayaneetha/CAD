edfa07a9958cdf5998330b5858bd6f1ffe2c60bd
Thejan Rajapakshe <coder.clix@gmail.com>
2015-08-11 09:01:20 +0530

    Init

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://testing.brightron.net/CAD/index.php/';
+$config['base_url']	= 'http://testing.brightron.net/CAD/';
 
 /*
 |--------------------------------------------------------------------------
